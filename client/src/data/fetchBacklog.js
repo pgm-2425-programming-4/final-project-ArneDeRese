c1978ddc3258211197f9e5197a3a53cdf67dea55
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-05-15 11:37:42 +0200

    add fetchBacklog function for paginated backlog retrieval

---
+++ client/src/data/fetchBacklog.js
@@ -0,0 +1,15 @@
+import { API_TOKEN, API_URL } from "../constants/constants";
+
+
+export async function fetchBacklog(page, pageSize) {
+    const result = await fetch(
+        `${API_URL}/backlogs?pagination[page]=${page}&pagination[pageSize]=${pageSize}`,
+        {
+            headers: {
+                Authorization: `Bearer ${API_TOKEN}`,
+            },
+        }
+    );
+    const data = await result.json()
+    return data
+}
