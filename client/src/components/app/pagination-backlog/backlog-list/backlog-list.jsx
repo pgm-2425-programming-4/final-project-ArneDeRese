83b4a140d28cf91ef21e3dc4dca67b8060423db7
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-05-15 12:15:21 +0200

    data put in a table

--- client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
+++ client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
@@ -1,14 +1,22 @@
 export function BacklogList({ backlogs }) {
   return (
-    <ul>
-      {backlogs.map((backlog) => {
-        return (
-          <li key={backlog.id}>
-            <h1>{backlog.Title}</h1>
-            {backlog.description}
-          </li>
-        );
-      })}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+        </tr>
+      </thead>
+      <tbody>
+        {backlogs.map((backlog) => {
+          return (
+            <tr key={backlog.id}>
+              <td>{backlog.Title}</td>
+              <td>{backlog.description}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
   );
 }
