f893416eadc94f26f4f266f4fb717dad0a2e51f6
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-05-15 12:00:35 +0200

    app written

---
+++ client/src/components/app/App.jsx
@@ -0,0 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PaginatedBacklogList } from "./pagination-backlog/paginated-backlog-list";
+
+const queryClient = new QueryClient
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div>
+        <PaginatedBacklogList/>
+      </div>
+    </QueryClientProvider>
+  )
+}
+
+export default App
