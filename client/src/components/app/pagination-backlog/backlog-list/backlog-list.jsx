1136aa85eb70e2b353c6198c4a5b28803593bc48
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-05-15 12:08:20 +0200

    function name fixed

--- client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
+++ client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
@@ -1,10 +1,11 @@
-export function StudentList({ backlogs }) {
+export function BacklogList({ backlogs }) {
   return (
     <ul>
       {backlogs.map((backlog) => {
         return (
           <li key={backlog.id}>
-            {backlog.title} {backlog.description}
+            <h1>{backlog.Title}</h1>
+            {backlog.description}
           </li>
         );
       })}
