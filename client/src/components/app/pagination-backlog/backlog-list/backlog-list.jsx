a1ba4a47797618494d383d918c0c77a883b517a1
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-12 09:40:12 +0200

    status added in backlog and filter added

--- client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
+++ client/src/components/app/pagination-backlog/backlog-list/backlog-list.jsx
@@ -1,18 +1,23 @@
 export function BacklogList({ tasks }) {
+  const backlogTasks = tasks.filter(
+    (task) => task.statuses?.Name?.toLowerCase() === "backlog"
+  );
   return (
     <table>
       <thead>
         <tr>
           <th>Title</th>
           <th>Description</th>
+          <th>Status</th>
         </tr>
       </thead>
       <tbody>
-        {tasks.map((task) => {
+        {backlogTasks.map((task) => {
           return (
             <tr key={task.id}>
               <td>{task.Title}</td>
               <td>{task.Description}</td>
+              <td>{task.statuses.Name}</td>
             </tr>
           );
         })}
