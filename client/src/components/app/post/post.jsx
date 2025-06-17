70cc050797a3b2e12c52978424d78a01d7aa3141
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 12:08:42 +0200

    Refactor post creation component and update route to use new form structure

--- client/src/components/app/post/post.jsx
+++ client/src/components/app/post/post.jsx
@@ -1,11 +1,43 @@
-import {  useState } from "react";
-import { API_URL } from "../../../constants/constants";
+import { useState } from "react";
+import { API_URL, API_TOKEN } from "../../../constants/constants.js";
 
-const Post = () => {
-  
-    fetch(API_URL + "/posts", {
-        method: "POST",
-        headers: { contentType: "application/json" },
-        body: JSON.stringify({ 
-    })})
+
+ export const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, };
+
+    fetch(`${API_URL}/tasks?populate=*`, {
+      method: 'POST',
+      headers: { "Content-Type": "application/json", 
+        Authorization: `Bearer ${API_TOKEN}`,
+       },
+      body: JSON.stringify(blog)
+  })}
+
+  return (
+    <div className="create">
+      <h2>Add a New task</h2>
+      <form onSubmit={handleSubmit}>
+        <label> title:</label>
+        <input 
+          type="text" 
+          required 
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>discription:</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
+        <button>Add Blog</button>
+      </form>
+    </div>
+  );
 }
+ 
