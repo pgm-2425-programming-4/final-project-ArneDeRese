e118c78934b5d9692fdd86857bea617f39b72a9b
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 21:41:34 +0200

    files merged and linked

--- client/src/routes/board/$board.jsx
+++ client/src/routes/board/$board.jsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { Board } from '../../components/app/board/board'
+import { createFileRoute, Link } from '@tanstack/react-router'
 import { fetchTaks } from '../../data/fetchTask'
+import { TaskList } from '../../components/app/tasks/task-list'
 
 export const Route = createFileRoute('/board/$board')({
   loader: async () => {
@@ -13,7 +13,35 @@ export const Route = createFileRoute('/board/$board')({
 function RouteComponent() {
   const { tasks } = Route.useLoaderData();
 
+  const statusOrder = ["done", "in progress", "ready for review", "to do"];
+  const filteredTasks = tasks.filter(
+    (task) => task.statuses?.Name?.toLowerCase() !== "backlog"
+  );
+  const groupedTasks = statusOrder.reduce((acc, status) => {
+    acc[status] = filteredTasks.filter(
+      (task) => task.statuses?.Name?.toLowerCase() === status
+    );
+    return acc;
+  }, {});
+
   return (
-    <Board tasks={tasks} />
+    <ul>
+      {statusOrder.map((status) => (
+        <li key={status}>
+          <h2>
+            <strong>{status}</strong>
+          </h2>
+          <ul>
+            {groupedTasks[status].map((task) => (
+              <li key={task.id}>
+                <Link to={`/board/task/${task.id}`}>
+                  {task.Title}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </li>
+      ))}
+    </ul>
   );
 }
