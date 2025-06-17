aa0fa17551b4344993c9b949da1baa71894d8863
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 19:17:53 +0200

    usestate/useEffect replaced with loader

--- client/src/routes/board/$board.jsx
+++ client/src/routes/board/$board.jsx
@@ -1,22 +1,17 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Board } from '../../components/app/board/board'
 import { fetchTaks } from '../../data/fetchTask'
-import { useEffect, useState } from "react"
 
 export const Route = createFileRoute('/board/$board')({
+  loader: async () => {
+    const data = await fetchTaks();
+    return { tasks: data.data || [] };
+  },
   component: RouteComponent,
-})
+});
 
 function RouteComponent() {
-  const [tasks, setTasks] = useState([]);
-
-  useEffect(() => {
-    async function loadTasks() {
-      const data = await fetchTaks();
-      setTasks(data.data || []);
-    }
-    loadTasks();
-  }, []);
+  const { tasks } = Route.useLoaderData();
 
   return (
     <Board tasks={tasks} />
