86eb9aba0b1947d01fa9fd53f562229c02e65416
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 17:42:32 +0200

    taskDetail made

---
+++ client/src/components/app/tasks/taskDetail.jsx
@@ -0,0 +1,28 @@
+export function BacklogList({ tasks }) {;
+  return (
+    <div>
+    <table>
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Description</th>
+          <th>Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        {tasks.map((task) => {
+          return (
+            <tr key={task.id}>
+              <td>{task.Title}</td>
+              <td>{task.Description}</td>
+              <td>{task.statuses.Name}</td>
+              <td>{task.projects.Title}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+    </div>
+    
+  );
+}
