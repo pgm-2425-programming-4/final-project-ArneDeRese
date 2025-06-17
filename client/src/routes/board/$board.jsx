78f5ecf9a5fa4292c6fb7e784a67e1db073d5901
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 18:11:44 +0200

    route updated

--- client/src/routes/board/$board.jsx
+++ client/src/routes/board/$board.jsx
@@ -1,12 +1,24 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Board } from '../../components/app/board/board'
+import { fetchTaks } from '../../data/fetchTask'
+import { useEffect, useState } from "react"
 
 export const Route = createFileRoute('/board/$board')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    async function loadTasks() {
+      const data = await fetchTaks();
+      setTasks(data.data || []);
+    }
+    loadTasks();
+  }, []);
+
   return (
-    <Board />
-  )
+    <Board tasks={tasks} />
+  );
 }
