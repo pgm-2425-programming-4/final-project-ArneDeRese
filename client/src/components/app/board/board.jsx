a043003a892e46eadfa45d773f146a2d813f9541
«ArneDeRese» <arne.derese@student.arteveldehs.be>
2025-06-17 19:16:50 +0200

    refactor: streamline task grouping and rendering in Board component

--- client/src/components/app/board/board.jsx
+++ client/src/components/app/board/board.jsx
@@ -1,28 +1,30 @@
 import { TaskList } from "../tasks/task-list";
 
 export function Board({ tasks }) {
-  function hasTasksWithStatus(tasks, status) {
-    return tasks.some(
-      (task) => task.statuses?.Name?.toLowerCase() === status.toLowerCase()
+  const statusOrder = ["done", "in progress", "ready for review", "to do"];
+  const filteredTasks = tasks.filter(
+    (task) => task.statuses?.Name?.toLowerCase() !== "backlog"
+  );
+  const groupedTasks = statusOrder.reduce((acc, status) => {
+    acc[status] = filteredTasks.filter(
+      (task) => task.statuses?.Name?.toLowerCase() === status
     );
-  }
+    return acc;
+  }, {});
 
   return (
     <ul>
-      <li>
-        {hasTasksWithStatus(tasks, "Done") && <TaskList />}
-      </li>
-      <li>
-        {hasTasksWithStatus(tasks, "In progress") && <TaskList />}
-      </li>
-      <li>
-        {hasTasksWithStatus(tasks, "Ready for review") && <TaskList />}
-      </li>
-      <li>
-        {hasTasksWithStatus(tasks, "To do") && <TaskList />}
-      </li>
+      {statusOrder.map((status) => (
+        <li key={status}>
+          <h2><strong>{(status)}</strong></h2>
+          <TaskList tasks={groupedTasks[status]} />
+        </li>
+      ))}
     </ul>
   );
 }
+
+
+
 
 
